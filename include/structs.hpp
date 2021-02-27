399e8a8877bacd5d53cf461a935d340a74aeab88
Ramil Safin <RamilSafNab1996@gmail.com>
2021-02-27 13:59:05 +0300

    Update structs

--- include/structs.hpp
+++ include/structs.hpp
@@ -6,17 +6,17 @@
 #include <iostream>
 
 // Не советуется использовать пространства имен в заголовочных файлах!
-// Заголовочные файлы могут быть использованы во многих
-// файлах с исходным кодом, что потенциально может вызвать проблемы коллизии имен.
-// using namespace std;
+// Заголовочные файлы могут быть использованы во многих файлах с исходным кодом,
+// что потенциально может вызвать проблемы коллизии имен.
+// using namespace std;  // <- ошибки нет, все функции и объекты из std будут доступны в текущем пространстве имен
 
 // struct <название>
 struct Student {
   // поля структуры:
-  int id{-1};             // id = -1
-  int age{};              // age = 0
-  std::string full_name;  // full_name = ""
-  double avg_score;       // avg_score = "мусор"
+  int id;            // id = <мусор>
+  int age;
+  std::string name;  // name = ""
+  double avg_score;
 };
 
 // вложенность стуктур
@@ -36,85 +36,74 @@ void print_details(Student const &student);
 struct University {
  public:  // публичный спецификатор доступа - поля и методы являются открытыми для использования
 
-  // конструктор по-умолчанию
+  // конструктор по-умолчанию - специальный тип метода, который выполняется при создании объекта структуры
   University() : ranking_{0} /* список инициализации полей */ {
-
-    pointer_ = new int{1};  // выделение памяти в куче (нужно будет высвободить память)
-
     std::cout << "University()" << std::endl;
+    pointer_ = new int{1};  // выделение памяти в куче (нужно будет высвободить память)
   }
 
-  // explicit - запрет неявного преобразования string в University
-  explicit University(std::string const &name) : name_{name}, ranking_{0} {
-    std::cout << "explicit University(name)" << std::endl;
+  // деструктор - специальный тип метода, который выполняется при удалении объекта структуры
+  ~University() {
+    std::cout << "~University()" << std::endl;
+
+    delete pointer_;     // освобождение выделенной памяти перед уничтожением объекта
+    pointer_ = nullptr;  // не забываем обнулить указатель!!!
   }
 
-  /* explicit */ University(int ranking) : ranking_{ranking} {
-    std::cout << "University(ranking)" << std::endl;
-  }
+  // explicit конструктор - запрет неявного преобразования string в University
+  explicit University(std::string const &name);
 
-  University(std::string const &name, int ranking)
-      : name_{name}, ranking_{ranking} {
-    std::cout << "University(name, ranking)" << std::endl;
-  }
+  // implicit конструктор, запрета на неявное преобразование int в University нет
+  /* explicit */ University(int ranking);
 
-  // методы
+  // конструктор от 2 параметров
+  University(std::string const &name, int ranking);
+
+  // публичные методы
 
   // const методы - гарантируют, что поля структуры не изменятся
-  std::string GetName() const {
-    // name_ = "KFU";  // <- ошибка компиляции
-    return name_;
-  }
+  int GetId() const;
+
+  std::string GetName() const;
 
   // возвращаемое значение - это копия поля
   int GetRanking() const;  // <- объявление метода, определение лежит в файле struct.cpp
 
   // возвращение ссылки
-  std::string &GetNameRef() /* const - нельзя, нет гарантии, что name_ не изменится */ {
-    ranking_ = 0;  // можно изменять поля объекта, но не стоит, это плохой код
-    return name_;  // по ссылке можно будет изменять поле name_ у объекта типа University
-  }
+  std::string &GetNameRef();
 
   // ссылка на константу (константная ссылка)
-  std::string const &GetNameConstRef() const /* const - уже есть гарантии неизменности name_ */ {
-    // private_function();  // <- ошибка компиляции
-    private_const_function();
-
-    return name_;
-  }
+  std::string const &GetNameConstRef() const;
 
   // ключевое слово this - неявный указатель на объект структуры
-  University &GetThisRef(/* University* this */) {
+  University &GetThisRef(/* University* this */);
 
-    std::string name = this->name_;
-    // эквивалентно: std::string name = name_;
-
-    return *this;  // разыменуем и получаем адрес объекта в памяти
-    // который можем вернуть как ссылку (можно и указатель вернуть)
+  // статический метод структуры
+  static int CurrentID() {
+    // можем работать только со статическими полями
+    return University::ID_;  // можно просто return ID_
   }
 
-  // деструктор - специальный тип метода, который выполняется при удалении объекта структуры
-  // не важно откуда - из стека или кучи
-  ~University() {
+ public: // публичные поля
 
-    delete pointer_;  // освобождение выделенной памяти перед уничтожением объекта
-
-    pointer_ = nullptr;  // не забываем!!!
-
-    std::cout << "~University()" << std::endl;
-  }
+  static int ID_;  // статическое поле - общее для всех объектов структуры
+  // если не инициализировать ID_, то он будет хранить "мусор"
 
  private: // приватный спецификатор доступа - поля и методы являются закрытыми для использования вне структуры
 
   // правила наименования полей: добавление _ в конец названия (Google C++ Style Guide)
+  int id_;
   std::string name_;
   std::vector<Faculty> faculties_;
   int ranking_;
 
-  int *pointer_{nullptr};
+  int *pointer_{nullptr};  // инициализация nullptr
 
-  // приватные методы, вызов которых возможен только внутри структуры
+  // приватные методы (вызов которых возможен только внутри структуры)
   void private_function() {}
   void private_const_function() const {}
 };
 
+// инициализация статического поля структуры
+// осущетствляется за пределами структуры (либо в заголовочном файле, либо в cpp файле)
+int University::ID_ = 0;
--- src/structs.cpp
+++ src/structs.cpp
@@ -12,89 +12,108 @@ using namespace std;
 int main() {
 
   {
-    Student student;  // { id = -1, age = 0, full_name = "", avg_score = "мусор" }
+    Student student;  // { id = <мусор>, age = <мусор>, name = "", avg_score = <мусор> }
 
-    // инициализация
+    // инициализация структуры (aggregate initialization)
     student = {0, 24, "Ramil Safin", 92.6};
 
     // доступ к полям (оператор .)
-    const int ID = student.id;  // чтение
-    student.avg_score -= 0.3;   // запись
+    const double avg_score = student.avg_score;  // чтение
+    student.avg_score = avg_score - 0.3;         // запись
 
     // копирование
-    Student const student_copy = student;  // { id = 0, age = 24, full_name = "Ramil Safin", avg_score = 92.3 }
+    Student const student_copy = student;  // { id = 0, age = 24, name = "Ramil Safin", avg_score = 92.3 }
+
+    // student_copy.name = "";  // <- ошибка компиляции
 
     // указатель и ссылка
 
     Student *ptr_to_student = &student;
 
     // оператор обращения к полям структуры через указатель (->)
-    ptr_to_student->age = 32;  // эквивалентно (*ptr_to_student).avg_score = new_score;
-
+    ptr_to_student->age = 32;  // эквивалентно (*ptr_to_student).age = 32;
     update_score(ptr_to_student, 86);
 
-    print_details(student);
-
     Student &ref_to_student = student;
-
-    update_score(ref_to_student, 90);
-
-    print_details(ref_to_student);
+    update_score(ref_to_student, 90);  // ссылка
+    print_details(ref_to_student /* ссылка на const */);
   }
 
   {  // вложенные структуры
 
     Faculty faculty;
 
-    faculty.name = "ITIS";
-
-    faculty.students = {
-        {0, 24, "Student 0", 90.2},
-        {1, 23, "Student 1", 92.3},
-        {2, 24, "Student 2", 85.9}
+    // инициализация (aggregate initialization)
+    faculty = {
+        "ITIS",
+        {
+            {0, 24, "Student 0", 90.2},
+            {1, 23, "Student 1", 92.3},
+            {2, 25, "Student 2", 85.9}
+        }
     };
 
+    // доступ к вложенным полям структуры
     int const age = faculty.students[0].age;
   }
 
-  {  // приватные публичные поля и методы структуры
+  {  // конструкторы и деструкторы
+
+    { // вошли в новую область видимости
+
+      University u1;            // конструктор по-умолчанию (объект создается на стеке)
+      u1.~University();         // ручной вызов деструктора (объект НЕ удаляется со стека)
+
+      // здесь мы ещё можем продолжать использовать u1
+
+    }  // вышли из области видимости => автоматический вызов деструктора u1
+
+    University u2("KFU");  // explicit конструктор по name
+
+    // University u2_str = string("KFU");  // <- ошибка компиляции (неявное приведение типа string к University)
+    University u2_str = static_cast<University>(string("KFU"));  // явное приведение типа
+
+    University u3(1);  // НЕ explicit конструктор по ranking
+    University u3_int = 1;    // ОК, вызов конструктора с ranking (неявное преобразование типа int в University)
+
+    University u4("KFU", 1);  // конструктор по name и ranking
+  }
+
+  {  // приватные/публичные поля и методы структуры
 
     University uni;  // вызов конструктора по-умолчанию (создание объекта класса uni)
 
     // uni.name_ = "";  // <- ошибка компиляции (поле name_ приватное)
 
-    // вызов методов объекта, неявный указатель this
-
+    // вызов публичных методов
     string name = uni.GetName();  // копия поля name_
-
     int const ranking = uni.GetRanking();
 
     string &name_ref = uni.GetNameRef();  // ссылка на поле name_
-    name_ref = "";  // ОК
+    name_ref = "";  // ОК, теперь uni.name_ = ""
 
     string const &name_const_ref = uni.GetNameConstRef();  // ссылка на неизменяемое поле name_
     // name_const_ref = "";  // <- ошибка компиляции
 
+    // вызов приватных функций - невозможен
     // uni.private_function();  // <- ошибка компиляции
-
-    auto& this_ref = uni.GetThisRef();
-    // компилятор записывает эту строку примерно так:
-    // GetThisRef(&uni) - т.е. компилятор передает указатель на объект (Python: self, Java: this)
   }
 
-  {  // конструкторы и деструкторы
+  {  // неявный указатель this
 
-    University u1;  // конструктор по-умолчанию (объект на стеке)
+    University uni;
 
-    u1.~University();  // ручной вызов деструктора (высвобождение памяти, выделенной в объекте)
+    auto &this_ref = uni.GetThisRef();
+    // компилятор записывает строку кода выше примерно следующим образом:
+    // GetThisRef(&uni) - т.е. компилятор передает указатель на объект неявным аргументом
+    // Ex.: Python: self (явный), Java: this (неявный)
+  }
 
-    University u2("KFU");  // explicit конструктор по name
-    // University u2_str = string("KFU");  // <- ошибка компиляции (неявное приведение типа string к University)
+  {  // статические методы и поля
 
-    University u3(1);  // не explicit конструктор по ranking
-    University u3_int = 1;    // вызов конструктора с ranking (неявное преобразование типа int в University)
+    int ID = University::ID_;  // получение значения статического поля структуры
 
-    University u4("KFU", 1);  // конструктор по name и ranking
+    int curr_id = University::CurrentID();  // вызов статического метода структуры
   }
 
   {  // создание объектов структуры на куче, деструктор
@@ -104,7 +123,8 @@ int main() {
     string name = u_ptr->GetName();
 
     delete u_ptr;  // ручной вызов деструктора и освобождение памяти
-  }
+
+  }  // при выходе из области видимости деструктор не вызовется (высвободится лишь память под указатель u_ptr)
 
   return 0;
 }
@@ -120,11 +140,57 @@ void update_score(Student *student, double new_score) {
 
 void print_details(const Student &student) {
   // student нужен только для чтения данных: id и пр.
-  std::cout << "Student: " << "ID = " << student.id << "\tName: " << student.full_name << endl;
+  std::cout << "Student: " << "ID = " << student.id << "\tName: " << student.name << endl;
 }
 
-// методы struct University из заголовочного файла
+// определение методов University из заголовочного файла
+
+// <название структуры>::<навзание метода>(<параметры>) : <список инициализации полей> { <тело метода> }
+// :: - оператор разрешение области, используется для идентификации и устранения неоднозначности идентификаторов
+
+University::University(const string &name) : University(name, 0) /* делигирующий конструктор */ {
+  std::cout << "explicit University(name)" << std::endl;
+}
+
+University::University(int ranking) : University("", ranking) {
+  std::cout << "University(ranking)" << std::endl;
+}
+
+University::University(const string &name, int ranking) : name_{name}, ranking_{ranking} {
+  // генерация идентификатора на базе статического поля структуры
+  id_ = ID_++;  // эквивалентно: id_ = ID_ и ID_ += 1
+}
+
+int University::GetId() const {
+  return id_;
+}
+
+std::string University::GetName() const {
+  // name_ = "KFU";  // <- ошибка компиляции
+  return name_;
+}
 
 int University::GetRanking() const {
   return ranking_;
+}
+
+std::string &University::GetNameRef() /* const - нельзя, нет гарантии, что name_ не изменится */ {
+  ranking_ = 0;  // можно изменять поля объекта, но не стоит, это плохой код
+  return name_;  // по ссылке можно будет изменять поле name_ у объекта типа University
+}
+
+std::string const &University::GetNameConstRef() const /* const - уже есть гарантии неизменности name_ */ {
+  // private_function();  // <- ошибка компиляции
+  private_const_function();
+
+  return name_;
+}
+
+University &University::GetThisRef() {
+
+  std::string name = this->name_;
+  // эквивалентно: std::string name = name_;
+
+  return *this;  // разыменуем указатель и получаем адрес объекта в памяти
+  // который можем вернуть как ссылку (можно и указатель вернуть)
 }
